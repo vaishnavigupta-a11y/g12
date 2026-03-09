1b1c147802b14baf14c9272228e100706af6b1e5
vaishnavi <vaishnavi.gupta@gammaedge.io>
2026-03-09 13:25:39 +0530

    c5

---
+++ important.py
@@ -0,0 +1,20 @@
+print("lerns force commad ")# git clone
+# chnages
+# git sttaus
+# git add .
+# git sttsus 
+# git commit -m "c1"
+# git push -u origin main
+
+
+# now ralise last commit glt tha
+# then git reset --hard head~1
+# deketung lats cimmit ->only locals  edlt hua now wnat github se dlt hojye
+# git push eroror hsow ki rmeote or local ki file match nahi horhi 
+ 
+#  firtsoption1
+#  force dlt need 
+# git push -u origin main --force
+# fircefully local changes cimmit rmeote me cale jyeneg 
+
+
